bc92a691183c2c21bb8d9e0490e9e80b5478bfbf
Nikita Surnachev <nikita.surnachev03@gmail.com>
2019-03-28 00:02:06 +0300

    Model fields, extended shout info

--- Slicer/admin.py
+++ Slicer/admin.py
@@ -2,7 +2,20 @@ from django.contrib import admin
 
 from .models import ImageSeries
 
+# admin.site.register(ImageSeries)
 
 @admin.register(ImageSeries)
 class ImageSeriesAdmin(admin.ModelAdmin):
-    readonly_fields = ('voxel_file', 'patient_id', 'study_uid', 'series_uid')
+    readonly_fields = ('voxel_file', 'patient_id', 'study_uid', 'series_uid', 'BitsAllocated',
+        'BitsStored', 'Columns', 'HighBit', 'InStackPositionNumber', 'PixelRepresentation', 'Rows',
+        'SamplesPerPixel', 'TemporalPositionIndex', 'AccessionNumber', 'AcquisitionDate', 'AcquisitionDateTime',
+        'AcquisitionTime', 'BodyPartExamined', 'ContentDate', 'ContentTime', 'ConvolutionKernel',
+        'DerivationDescription', 'DeviceSerialNumber', 'ExposureModulationType', 'FilterType',
+        'InstitutionAddress', 'InstitutionName', 'InstitutionalDepartmentName', 'IssuerOfPatientID',
+        'Manufacturer', 'ManufacturerModelName', 'Modality', 'PatientAge', 'PatientBirthDate', 'PatientID',
+        'PatientPosition', 'PatientSex', 'PerformedProcedureStepID', 'PerformedProcedureStepStartDate',
+        'PerformedProcedureStepStartTime', 'PhotometricInterpretation', 'PositionReferenceIndicator',
+        'ProtocolName', 'RetrieveAETitle', 'RotationDirection', 'ScanOptions', 'ScheduledProcedureStepEndDate',
+        'ScheduledProcedureStepEndTime', 'ScheduledProcedureStepStartDate', 'ScheduledProcedureStepStartTime',
+        'SeriesDate', 'SeriesDescription', 'SeriesTime', 'SoftwareVersions', 'SpecificCharacterSet', 'StackID',
+        'StationAETitle', 'StationName', 'StudyDate', 'StudyID', 'StudyStatusID', 'StudyTime')
--- Slicer/models.py
+++ Slicer/models.py
@@ -20,6 +20,66 @@ class ImageSeries(models.Model):
     study_uid = models.CharField(max_length=64)
     series_uid = models.CharField(max_length=64)
 
+    AccessionNumber = models.CharField(max_length=128)
+    AcquisitionDate = models.CharField(max_length=128)
+    AcquisitionDateTime = models.CharField(max_length=128)
+    AcquisitionTime = models.CharField(max_length=128)
+    BodyPartExamined = models.CharField(max_length=128)
+    ContentDate = models.CharField(max_length=128)
+    ContentTime = models.CharField(max_length=128)
+    ConvolutionKernel = models.CharField(max_length=128)
+    DerivationDescription = models.CharField(max_length=128)
+    DeviceSerialNumber = models.CharField(max_length=128)
+    ExposureModulationType = models.CharField(max_length=128)
+    FilterType = models.CharField(max_length=128)
+    InstitutionAddress = models.CharField(max_length=128)
+    InstitutionName = models.CharField(max_length=128)
+    InstitutionalDepartmentName = models.CharField(max_length=128)
+    IssuerOfPatientID = models.CharField(max_length=128)
+    Manufacturer = models.CharField(max_length=128)
+    ManufacturerModelName = models.CharField(max_length=128)
+    Modality = models.CharField(max_length=128)
+    PatientAge = models.CharField(max_length=128)
+    PatientBirthDate = models.CharField(max_length=128)
+    PatientID = models.CharField(max_length=128)
+    PatientPosition = models.CharField(max_length=128)
+    PatientSex = models.CharField(max_length=128)
+    PerformedProcedureStepID = models.CharField(max_length=128)
+    PerformedProcedureStepStartDate = models.CharField(max_length=128)
+    PerformedProcedureStepStartTime = models.CharField(max_length=128)
+    PhotometricInterpretation = models.CharField(max_length=128)
+    PositionReferenceIndicator = models.CharField(max_length=128)
+    ProtocolName = models.CharField(max_length=128)
+    RetrieveAETitle = models.CharField(max_length=128)
+    RotationDirection = models.CharField(max_length=128)
+    ScanOptions = models.CharField(max_length=128)
+    ScheduledProcedureStepEndDate = models.CharField(max_length=128)
+    ScheduledProcedureStepEndTime = models.CharField(max_length=128)
+    ScheduledProcedureStepStartDate = models.CharField(max_length=128)
+    ScheduledProcedureStepStartTime = models.CharField(max_length=128)
+    SeriesDate = models.CharField(max_length=128)
+    SeriesDescription = models.CharField(max_length=128)
+    SeriesTime = models.CharField(max_length=128)
+    SoftwareVersions = models.CharField(max_length=128)
+    SpecificCharacterSet = models.CharField(max_length=128)
+    StackID = models.CharField(max_length=128)
+    StationAETitle = models.CharField(max_length=128)
+    StationName = models.CharField(max_length=128)
+    StudyDate = models.CharField(max_length=128)
+    StudyID = models.CharField(max_length=128)
+    StudyStatusID = models.CharField(max_length=128)
+    StudyTime = models.CharField(max_length=128)
+
+    BitsAllocated = models.IntegerField(default=0)
+    BitsStored = models.IntegerField(default=0)
+    Columns = models.IntegerField(default=0)
+    HighBit = models.IntegerField(default=0)
+    InStackPositionNumber = models.IntegerField(default=0)
+    PixelRepresentation = models.IntegerField(default=0)
+    Rows = models.IntegerField(default=0)
+    SamplesPerPixel = models.IntegerField(default=0)
+    TemporalPositionIndex = models.IntegerField(default=0)
+
     @property
     def voxels(self):
         with self.voxel_file as f:
@@ -51,6 +111,14 @@ class ImageSeries(models.Model):
         self.patient_id = dicom_datasets[0].PatientID
         self.study_uid = dicom_datasets[0].StudyInstanceUID
         self.series_uid = dicom_datasets[0].SeriesInstanceUID
+
+        for att in dir(dicom_datasets[0]):
+            try:
+                setattr( self, att, getattr(dicom_datasets[0], att) )
+                print("DEBUG IN ATTR: OK")
+            except:
+                print("DEBUG IN ATTR: Error")
+
         super(ImageSeries, self).save(*args, **kwargs)
         
     def delete(self, *args, **kwargs):

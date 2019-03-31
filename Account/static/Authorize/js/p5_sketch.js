880c7d35790160ac9e37604574536e153218d1f3
Nikita Surnachev <nikita.surnachev03@gmail.com>
2019-03-31 22:37:44 +0300

    Animation

--- Account/static/Authorize/js/p5_sketch.js
+++ Account/static/Authorize/js/p5_sketch.js
@@ -1,18 +1,32 @@
-let Width =  window.innerWidth - 20;
-let heigth = 500;
+// let windowWidth =  window.innerWidth - 20;
+// let heigth = 500;
 let vers = new Array();
 let edges = new Array();
-let VerR = 15;
-let AbsVel = 0.3;
-let maxVel = 0.9, stableVel = 0.22;
+let VerR = 9;
+let AbsVel = 0.1;
+let maxVel = 0.5, stableVel = 0.08;
 let NumVers = 80;
 let minConnectDistance = 80;
 let mouseNearA = 20;
 let mouseNearK = 2;
 let lineWidth = 0;
+let cnv, bg;
+
+let backgrounds_dark = ['2bg.jpg', '3bg.jpg', '5bg.jpg', '6bg.jpg']
+let backgrounds_bright = ['1bg.jpg', '4bg.jpg']
+
+let standartVertexColor;
 
 function getRndInteger(min, max) {
     return Math.floor(random(min, max)) + min;
+}
+
+function getSuperfluousR(dx, dy, l) {
+    let squareSide = VerR / 2;
+    let rx = (dx * squareSide) / l;
+    let ry = (dy * squareSide) / l;
+    console.log(rx, ry);
+    return {"x": rx, "y": ry};
 }
 
 // class Edge {
@@ -43,24 +57,48 @@ function getDistanceToDot(v1, x, y) {
 
 function generateEllipses(n) {
     for (let i = 0; i < n; i++) {
-        vers.push(new Vertex(random(VerR, Width - VerR), random(VerR, height - VerR), random(-AbsVel, AbsVel), random(-AbsVel, AbsVel),
-            {"r": random(255), "g": random(255), "b": random(255)}));
+        vers.push(new Vertex(random(VerR, windowWidth - VerR), random(VerR, windowHeight - VerR), random(-AbsVel, AbsVel), random(-AbsVel, AbsVel),
+            standartVertexColor));
     }
-}
-
-function setup() {
-    createCanvas(Width, 500);
-    generateEllipses(NumVers);
-    // generateEdges(10);
 }
 
 function mousePressed() {
     vers.push(new Vertex(mouseX, mouseY, random(-AbsVel, AbsVel), 
-        random(-AbsVel, AbsVel), {"r": random(255), "g": random(255), "b": random(255)}))
+        random(-AbsVel, AbsVel), standartVertexColor))
 }
 
-function draw() {    
-    background(255);
+function setup() {
+    let bgs = backgrounds_bright.concat(backgrounds_dark);
+    let bg_name = bgs[getRndInteger(0, bgs.length)];
+    bg = loadImage("/static/Authorize/img/" + bg_name);
+
+    if (backgrounds_bright.includes(bg_name)) {
+        standartVertexColor = {"r": 0, "g": 124, "b": 118};        
+    } else {
+        standartVertexColor = {"r": 0, "g": 255, "b": 234};
+    }
+
+    cnv = createCanvas(windowWidth, windowHeight);
+    cnv.style('display', 'block');
+    cnv.style('z-index', '900')
+    centerCanvas();
+    // cnv.parent('animation_holder');
+    generateEllipses(NumVers);
+    // generateEdges(10);
+}
+
+function centerCanvas() {
+    var x = (windowWidth - width) / 2;
+    var y = (windowHeight - height) / 2;
+    cnv.position(x, y);
+  }  
+
+function windowResized() {
+    resizeCanvas(windowWidth, windowHeight);
+}
+
+function draw() {  
+    background(bg);  
     for (let i = 0; i < vers.length; i++) {
         fill(vers[i].color.r, vers[i].color.g, vers[i].color.b);
         ellipse(vers[i].x, vers[i].y, VerR, VerR);
@@ -79,10 +117,10 @@ function draw() {
             }  
         }
 
-        if (vers[i].x - VerR <= 0 || vers[i].x + VerR >= Width) {
+        if (vers[i].x - VerR <= 0 || vers[i].x + VerR >= windowWidth) {
             vers[i].vx = -vers[i].vx
         }
-        if (vers[i].y - VerR <= 0 || vers[i].y + VerR >= height) {
+        if (vers[i].y - VerR <= 0 || vers[i].y + VerR >= windowHeight) {
             vers[i].vy = -vers[i].vy
         }
 
@@ -91,9 +129,25 @@ function draw() {
     }
     for (let i = 0; i < vers.length; i++) {
         for (let j = i + 1; j < vers.length; j++) {
-            if (getVerDistance(vers[i], vers[j]) <= minConnectDistance) {
+            let verDis = getVerDistance(vers[i], vers[j]);
+            if (verDis <= minConnectDistance) {
                 if (lineWidth == 0) {
-                    line(vers[i].x, vers[i].y, vers[j].x, vers[j].y);
+                    stroke(153);
+                    let dr = getSuperfluousR(Math.abs(vers[i].x - vers[j].x),
+                        Math.abs(vers[i].y - vers[j].y), verDis)
+                    if (vers[i].x < vers[j].x) {
+                        if (vers[i].y < vers[j].y) {
+                            line(vers[i].x + dr.x, vers[i].y + dr.y, vers[j].x - dr.x, vers[j].y - dr.y);
+                        } else {
+                            line(vers[i].x + dr.x, vers[i].y - dr.y, vers[j].x - dr.x, vers[j].y + dr.y);
+                        }
+                    } else {
+                        if (vers[i].y < vers[j].y) {
+                            line(vers[i].x - dr.x, vers[i].y + dr.y, vers[j].x + dr.x, vers[j].y - dr.y);
+                        } else {
+                            line(vers[i].x - dr.x, vers[i].y - dr.y, vers[j].x + dr.x, vers[j].y + dr.y);                            
+                        }
+                    }
                 } else {
                     fill(random(255), random(255), random(255));
                     quad(vers[i].x - lineWidth / 2, vers[i].y - lineWidth / 2,
